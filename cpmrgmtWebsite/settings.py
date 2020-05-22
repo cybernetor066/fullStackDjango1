438f65b63471df55711fb1fcc99aebafc4badd12
Anetor Eguakhide Kunle <Delwaziri@hotmail.com>
2020-05-22 01:39:14 +0100

    Third Commit1

--- cpmrgmtWebsite/settings.py
+++ cpmrgmtWebsite/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%s' % os.environ['MY_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['127']
